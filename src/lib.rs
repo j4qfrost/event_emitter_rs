5ab3f7a33457c4ad5d4969b35094bbc90b4b6a11
Dylan-Kerler <dylanphoon@gmail.com>
2020-08-06 20:55:38 +0100

    Fix code display

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,112 @@
 /*! 
-    A simple EventEmitter implementation.
+A simple EventEmitter implementation.
 
-    Allows you to subscribe to events with callbacks and also fire those events.
-    Events are in the form of (strings, value) and callbacks are in the form of closures that take in a value parameter.
+Allows you to subscribe to events with callbacks and also fire those events.
+Events are in the form of (strings, value) and callbacks are in the form of closures that take in a value parameter.
 
-    # Getting Started
+# Getting Started
 
-    ```
-    use event_emitter_rs::EventEmitter;
-    let mut event_emitter = EventEmitter::new();
+```
+use event_emitter_rs::EventEmitter;
+let mut event_emitter = EventEmitter::new();
 
-    // This will print <"Hello world!"> whenever the <"Say Hello"> event is emitted
-    event_emitter.on("Say Hello", |value: ()| println!("Hello world!"));
-    event_emitter.emit("Say Hello", ()); 
-    // >> "Hello world!"
-    ```
+// This will print <"Hello world!"> whenever the <"Say Hello"> event is emitted
+event_emitter.on("Say Hello", |value: ()| println!("Hello world!"));
+event_emitter.emit("Say Hello", ()); 
+// >> "Hello world!"
+```
 
-    # Basic Usage
+# Basic Usage
 
-    We can emit values of any type so long as they implement the serde Serialize and Deserialize traits.
-    A single EventEmitter instance can have listeners to values of multiple types.
+We can emit values of any type so long as they implement the serde Serialize and Deserialize traits.
+A single EventEmitter instance can have listeners to values of multiple types.
 
-    ```
-    use event_emitter_rs::EventEmitter;
-    use serde::{Deserialize, Serialize};
-    let mut event_emitter = EventEmitter::new();
+```
+use event_emitter_rs::EventEmitter;
+use serde::{Deserialize, Serialize};
+let mut event_emitter = EventEmitter::new();
 
-    event_emitter.on("Add three", |number: f32| println!("{}", number + 3.0));
-    event_emitter.emit("Add three", 5.0 as f32); 
-    event_emitter.emit("Add three", 4.0 as f32);
-    // >> "8.0"
-    // >> "7.0"
+event_emitter.on("Add three", |number: f32| println!("{}", number + 3.0));
+event_emitter.emit("Add three", 5.0 as f32); 
+event_emitter.emit("Add three", 4.0 as f32);
+// >> "8.0"
+// >> "7.0"
 
-    // Using a more advanced value type such as a struct by implementing the serde traits
-    #[derive(Serialize, Deserialize)]
-    struct Date {
-        month: String,
-        day: String,   
-    }
+// Using a more advanced value type such as a struct by implementing the serde traits
+#[derive(Serialize, Deserialize)]
+struct Date {
+    month: String,
+    day: String,   
+}
 
-    event_emitter.on("LOG_DATE", |date: Date| {
-        println!("Month: {} - Day: {}", date.month, date.day)
-    });
-    event_emitter.emit("LOG_DATE", Date { 
-        month: "January".to_string(), 
-        day: "Tuesday".to_string() 
-    }); 
-    // >> "Month: January - Day: Tuesday"
-    ```
+event_emitter.on("LOG_DATE", |date: Date| {
+    println!("Month: {} - Day: {}", date.month, date.day)
+});
+event_emitter.emit("LOG_DATE", Date { 
+    month: "January".to_string(), 
+    day: "Tuesday".to_string() 
+}); 
+// >> "Month: January - Day: Tuesday"
+```
 
-    Removing listeners is also easy
+Removing listeners is also easy
 
-    ```
-    use event_emitter_rs::EventEmitter;
-    let mut event_emitter = EventEmitter::new();
+```
+use event_emitter_rs::EventEmitter;
+let mut event_emitter = EventEmitter::new();
 
-    let listener_id = event_emitter.on("Hello", |_: ()| println!("Hello World"));
-    match event_emitter.remove_listener(&listener_id) {
-        Some(listener_id) => print!("Removed event listener!"),
-        None => print!("No event listener of that id exists")
-    }
-    ```
-    # Creating a Global EventEmitter
+let listener_id = event_emitter.on("Hello", |_: ()| println!("Hello World"));
+match event_emitter.remove_listener(&listener_id) {
+    Some(listener_id) => print!("Removed event listener!"),
+    None => print!("No event listener of that id exists")
+}
+```
+# Creating a Global EventEmitter
 
-    It's likely that you'll want to have a single EventEmitter instance that can be shared accross files;
+It's likely that you'll want to have a single EventEmitter instance that can be shared accross files;
 
-    After all, one of the main points of using an EventEmitter is to avoid passing down a value through several nested functions/types and having a global subscription service.
+After all, one of the main points of using an EventEmitter is to avoid passing down a value through several nested functions/types and having a global subscription service.
 
-    ```
-    // global_event_emitter.rs
-    use std::sync::Mutex;
-    use crate::EventEmitter;
+```
+// global_event_emitter.rs
+use std::sync::Mutex;
+use crate::EventEmitter;
 
-    // Use lazy_static! because the size of EventEmitter is not known at compile time
-    lazy_static! {
-        // Export the emitter with `pub` keyword
-        pub static ref EVENT_EMITTER: Mutex<EventEmitter> = Mutex::new(EventEmitter::new());
-    }
-    ```
+// Use lazy_static! because the size of EventEmitter is not known at compile time
+lazy_static! {
+    // Export the emitter with `pub` keyword
+    pub static ref EVENT_EMITTER: Mutex<EventEmitter> = Mutex::new(EventEmitter::new());
+}
+```
 
-    Then we can import this instance into multiple files.
+Then we can import this instance into multiple files.
 
-    ```
-    // main.rs
-    #[macro_use]
-    extern crate lazy_static;
+```
+// main.rs
+#[macro_use]
+extern crate lazy_static;
 
-    mod global_event_emitter;
-    use global_event_emitter::EVENT_EMITTER;
+mod global_event_emitter;
+use global_event_emitter::EVENT_EMITTER;
 
-    fn main() {
-        // We need to maintain a lock through the mutex so we can avoid data races
-        EVENT_EMITTER.lock().unwrap().on("Hello", |_: ()| println!("hello there!"));
-        EVENT_EMITTER.lock().unwrap().emit("Hello", ());
-    }
-    ```
+fn main() {
+    // We need to maintain a lock through the mutex so we can avoid data races
+    EVENT_EMITTER.lock().unwrap().on("Hello", |_: ()| println!("hello there!"));
+    EVENT_EMITTER.lock().unwrap().emit("Hello", ());
+}
+```
 
-    And in another file we can now listen to the <"Hello"> event in main.rs by adding a listener to the global event emitter.
+And in another file we can now listen to the <"Hello"> event in main.rs by adding a listener to the global event emitter.
 
-    ```
-    // some_random_file.rs
-    use crate::global_event_emitter::EVENT_EMITTER;
+```
+// some_random_file.rs
+use crate::global_event_emitter::EVENT_EMITTER;
 
-    fn random_function() {
-        // When the <"Hello"> event is emitted in main.rs then print <"Random stuff!">
-        EVENT_EMITTER.lock().unwrap().on("Hello", |_: ()| println!("Random stuff!"));
-    }
-    ```
+fn random_function() {
+    // When the <"Hello"> event is emitted in main.rs then print <"Random stuff!">
+    EVENT_EMITTER.lock().unwrap().on("Hello", |_: ()| println!("Random stuff!"));
+}
+```
 */
 
 //#[cfg(test)]
